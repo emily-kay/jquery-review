c1b3d62f81e8fdcb5e6edd1f93c930aaa85a32ac
Emily Kay <emilykay917@gmail.com>
2018-04-12 13:17:31 -0500

    Functional event handlers

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(onReady)
+//it is onReady not onReady() because you're calling the function itself/taking in the function not the result of the function
+
+console.log('jQuery Lecture');
+
+function onReady(){
+    console.log('jQuery is ready'); 
+    $('h1').css('color', 'gray');
+    $('h1').addClass('funny');
+
+    //getters and setters
+    console.log($('h2').text());
+    $('h2').text('What is your name?'); //get text value of h2, text is for most elements
+    
+    //inputs (also, getter and setter)
+    $('#nameInput').val('Name');
+    console.log($('#nameInput').val());//val is only for inputs
+    
+    //events
+    //event handler
+    $('#clickableButton').on('click', function() {
+        console.log($('#nameInput').val());
+        
+    });//clickableButton
+    $('#clickedButton').on('click', function() {
+        console.log($('#clickedButton').val());
+        
+    })
+}//onReady
